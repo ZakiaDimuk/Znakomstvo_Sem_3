08876d3ef9354435b0ccba9282712580d67b0c1b
ZakiaDim <dimukasheva97@mail.ru>
2023-03-21 18:59:10 +0500

    Progr1

---
+++ Progr1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.Write("Введите число: ");
+string number = Console.ReadLine()!;
+
+void NumberPal(string number){
+    if (number[0]==number[4] || number[1]==number[3]){
+        Console.WriteLine($"Число: {number} - да, является палиндромом.");
+    }
+    else 
+    Console.WriteLine($"Число: {number} - нет, не является палиндромом.");
+}
+
+if (number!.Length == 5){
+    NumberPal(number);
+}
+else
+Console.WriteLine($"Введено неверное число, введите верное число");
